6b9fb980f8462dafc68c24ebdc4b8c9b17b5a5fe
Shahreyar <shahreyaranwar97@gmail.com>
2026-09-01 15:59:40 +0530

    added Product entity with validation

---
+++ src/main/java/com/shopinventory/shop_inventory_management/model/Product.java
@@ -0,0 +1,40 @@
+package com.shopinventory.shop_inventory_management.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Product name is required")
+    private String name;
+
+    @NotNull(message = "Price is required")
+    @Positive(message = "Price must be greater than zero")
+    private Double price;
+
+    @NotNull(message = "Quantity is required")
+    @Min(value = 0, message = "Quantity cannot be negative")
+    private Integer quantity;
+
+    private String category;
+
+    public Product(String name, Double price, Integer quantity, String category) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.category = category;
+    }
+}

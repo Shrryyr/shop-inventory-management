0d15bc16e6f8fad3e9e3e689512cae1352d6fd5c
Shahreyar <shahreyaranwar97@gmail.com>
2026-09-01 15:16:35 +0530

    Initial commit: Spring Boot project skeleton

---
+++ src/main/java/com/shopinventory/shop_inventory_management/ShopInventoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.shopinventory.shop_inventory_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopInventoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shopinventory/shop_inventory_management/ShopInventoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopinventory.shop_inventory_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopInventoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

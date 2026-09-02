3be668e9c7f452ea78b3271c4b9ce7c106c3f9a0
Shahreyar <shahreyaranwar97@gmail.com>
2026-09-02 11:44:08 +0530

    added Sale entity linked to Product

---
+++ src/main/java/com/shopinventory/shop_inventory_management/model/Sale.java
@@ -0,0 +1,25 @@
+package com.shopinventory.shop_inventory_management.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Table(name = "sales")
+@Entity
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Product product;
+
+    private Integer quantitySold;
+
+    private LocalDateTime saleDate =  LocalDateTime.now();
+}
